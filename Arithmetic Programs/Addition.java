92b4030e00d34b2506d9f5ed6697bf6f7d484394
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-08-27 18:22:48 +0530

    Create Addition.java

---
+++ Arithmetic Programs/Addition.java
@@ -0,0 +1,13 @@
+package arithmetic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5  ,   b=7;
+		int sum = a+b;
+		System.out.println("Addition:-"+sum); 
+
+	}
+
+}
