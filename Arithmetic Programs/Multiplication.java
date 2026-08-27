ebf880eac1edc7724969b83694d3729df9250ec6
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-08-27 18:26:05 +0530

    Create Multiplication.java

---
+++ Arithmetic Programs/Multiplication.java
@@ -0,0 +1,15 @@
+package arithmetic;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5  ,   b=7;
+		int multiplication = a*b;
+		System.out.println("multiplication:-"+multiplication); 
+		
+	}
+
+
+}
+
