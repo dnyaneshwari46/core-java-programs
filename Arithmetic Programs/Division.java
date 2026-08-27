94cd6e286383ba941d79ee881c5332b71a45ef24
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-08-27 18:27:16 +0530

    Create Division.java

---
+++ Arithmetic Programs/Division.java
@@ -0,0 +1,13 @@
+package arithmetic;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10 ,   b=2;
+		int division = a/b;
+		System.out.println("division:-"+division); 
+		
+	}
+
+}
