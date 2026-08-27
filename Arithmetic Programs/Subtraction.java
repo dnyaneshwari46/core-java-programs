7aab82a71a532e3d0f914670b2692f3cf5c0a2b9
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-08-27 18:24:29 +0530

    Create Subtraction.java

---
+++ Arithmetic Programs/Subtraction.java
@@ -0,0 +1,13 @@
+package arithmetic;
+
+public class Substraction {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5  ,   b=7;
+		int substraction = a-b;
+		System.out.println("Substraction:-"+substraction); 
+
+	}
+
+
+}
