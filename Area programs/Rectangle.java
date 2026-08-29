9250dee35bf567b0aaa775edc54890f17c74502c
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-08-29 14:34:13 +0530

    Create Rectangle.java

---
+++ Area programs/Rectangle.java
@@ -0,0 +1,12 @@
+package area;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int length = 5, breadth = 7;
+		int area = length * breadth;
+		System.out.println("Area of rectangle:" + area);
+		}
+
+}
