4bd2f381e3487d528e6506d5d9cacedcfd83f0c6
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:30:08 +0530

    Create PositiveNegative.java

---
+++ Control Statments/PositiveNegative.java
@@ -0,0 +1,17 @@
+package controlStatements;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		int a=10;
+		if (a>0) {
+			System.out.println("Number is Positive");
+		}else {
+
+			System.out.println("Number is Negative");				
+		}
+	}
+
+}
+
+
