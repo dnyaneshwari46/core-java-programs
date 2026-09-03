f1a0dc9c0653635184b89a057d11a6fc87131db0
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:50:38 +0530

    Update and rename Circle.java to AreaOfCircle.java

---
+++ Area programs/AreaOfCircle.java
@@ -0,0 +1,12 @@
+package area;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		double pi=3.14 , r=4  ;
+		double area=pi*r*r;
+		System.out.println("Area of circle:-"+area);
+
+	}
+
+}
