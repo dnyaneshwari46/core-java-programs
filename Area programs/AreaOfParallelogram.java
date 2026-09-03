cd3fb4839d2fb5c3f20e4ac45cfc863b8a2c55e2
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:46:48 +0530

    Create AreaOfParallelogram.java

---
+++ Area programs/AreaOfParallelogram.java
@@ -0,0 +1,13 @@
+package area;
+public class AreaOfParallelogram {
+
+    public static void main(String[] args) {
+
+        double base = 12;
+        double height = 7;
+
+        double area = base * height;
+
+        System.out.println("Area of Parallelogram = " + area);
+    }
+}
