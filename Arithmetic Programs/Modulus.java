8076bf3f0dda2be8dad3ba97e98ac7650add53d6
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:40:47 +0530

    Create Modulus.java

---
+++ Arithmetic Programs/Modulus.java
@@ -0,0 +1,13 @@
+package arithmenticPrograms;
+public class Modulus {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 6;
+
+        int result = a % b;
+
+        System.out.println("Modulus = " + result);
+    }
+}
