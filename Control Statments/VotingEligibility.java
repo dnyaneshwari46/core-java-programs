e9bf642ae92daa9068cc2a94618ac65619fc5968
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:32:31 +0530

    Create VotingEligibility.java

---
+++ Control Statments/VotingEligibility.java
@@ -0,0 +1,14 @@
+package controlStatements;
+public class VotingEligibility {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Eligible for voting");
+        } else {
+            System.out.println("Not eligible for voting");
+        }
+    }
+}
