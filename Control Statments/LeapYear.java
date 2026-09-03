d252bb54b637e2e7eb7667e5bcc3875864bf6da5
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:27:03 +0530

    Create LeapYear.java

---
+++ Control Statments/LeapYear.java
@@ -0,0 +1,17 @@
+package controlStatements;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		 int year = 2021;
+	        
+	        if( (year % 400 == 0)|| (year % 4 == 0 && year % 100 != 0) ) {
+	            System.out.println(year + " is Leap Year");
+	        } else {
+	            System.out.println(year + " is Not Leap Year");
+	        }
+	    }
+	}
+	
+
+
