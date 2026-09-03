723b1054ac77277ae4eb402b245b00d3461fa08d
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:47:38 +0530

    Create AreaOfTrapezium.java

---
+++ Area programs/AreaOfTrapezium.java
@@ -0,0 +1,14 @@
+package area;
+public class AreaOfTrapezium {
+
+    public static void main(String[] args) {
+
+        double firstBase = 10;
+        double secondBase = 6;
+        double height = 5;
+
+        double area = 0.5 * (firstBase + secondBase) * height;
+
+        System.out.println("Area of Trapezium = " + area);
+    }
+}
