b3baff77e47a37e7043d6182eb6cfe3070c37baa
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-03 17:45:49 +0530

    Create AreaOfTriangle.java

---
+++ Area programs/AreaOfTriangle.java
@@ -0,0 +1,13 @@
+package area;
+public class AreaOfTriangle {
+
+    public static void main(String[] args) {
+
+        double base = 10;
+        double height = 8;
+
+        double area = 0.5 * base * height;
+
+        System.out.println("Area of Triangle = " + area);
+    }
+}
