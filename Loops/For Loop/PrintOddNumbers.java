e5432f34084a5aadf5329801a7f0e106582520df
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-11 12:23:27 +0530

    Create PrintOddNumbers.java

---
+++ Loops/For Loop/PrintOddNumbers.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class PrintOddNumbers {
+
+    public static void main(String[] args) {
+
+        // Print odd numbers from 1 to 20
+        for (int i = 1; i <= 20; i++) {
+
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
