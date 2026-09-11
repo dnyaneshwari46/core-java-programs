5b8dc58b84ddb1f674d1435415b2b52bcea3e6ff
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-11 13:02:58 +0530

    Create PrintNumbers.java

---
+++ Loops/Do While Loop/PrintNumbers.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        // Print numbers from 1 to 10
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
