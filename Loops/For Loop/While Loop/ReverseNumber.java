e6434776ee06010df041434327064b6b75f970b7
Dnyaneshwari Kamthe <kamthednyaneshwari130@gmail.com>
2026-09-11 12:42:08 +0530

    Create ReverseNumber.java

---
+++ Loops/For Loop/While Loop/ReverseNumber.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        int number = 12345;
+        int reverse = 0;
+
+        // Reverse the number
+        while (number != 0) {
+
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        System.out.println("Reversed number: " + reverse);
+    }
+}
